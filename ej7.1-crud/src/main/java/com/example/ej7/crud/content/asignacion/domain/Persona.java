a8a127f143118d46c95270fe9bae6a99661b38ac
Antonio Javier Palma Guerrero <antoniojavier.palma@bosonit.com>
2022-09-22 14:10:51 +0200

    empezando 7.1

---
+++ ej7.1-crud/src/main/java/com/example/ej7/crud/content/asignacion/domain/Persona.java
@@ -0,0 +1,11 @@
+package com.example.ej7.crud.content.asignacion.domain;
+
+public interface Persona {
+
+    String getNombre();
+    Integer getEdad();
+    String getPoblacion();
+    void setNombre(String s);
+    void setEdad(Integer e);
+    void setPoblacion(String p);
+}
---
+++ ej7.1-crud/src/main/java/com/example/ej7/crud/content/asignacion/domain/PersonaImpl.java
@@ -0,0 +1,44 @@
+package com.example.ej7.crud.content.asignacion.domain;
+
+public class PersonaImpl implements Persona {
+
+    private String nombre;
+    private Integer edad;
+    private String poblacion;
+
+    public PersonaImpl(String n, Integer e, String p){
+        nombre = n;
+        edad = e;
+        poblacion = p;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public Integer getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    @Override
+    public void setNombre(String s) {
+        nombre = s;
+    }
+
+    @Override
+    public void setEdad(Integer e) {
+        edad = e;
+    }
+
+    @Override
+    public void setPoblacion(String p) {
+        poblacion = p;
+    }
+}
